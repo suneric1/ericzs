210c30638bb75b99801bf26ea05ab198e98c7b67
Eric <suneric1@gmail.com>
2019-02-18 21:58:06 -0500

    config for heroku deploy

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist/ericzs'));
+app.use(express.static(path.join(__dirname, '/dist/ericzs')));
 
-app.get('/triango', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, '/dist/ericzs/assets/triango') }));
+app.get('/triango', (req, res) => res.sendFile('/dist/ericzs/assets/triango/index.html', { root: __dirname }));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/dist/ericzs/index.html')));
+app.get('*', (req, res) => res.sendFile('/dist/ericzs/index.html', { root: __dirname }));
 
 app.listen(port, () => console.log(`listening on port ${port}`));
